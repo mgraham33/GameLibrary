3425d7b2117d1b7ba9fe35db6fbf9a208532c2d0
Matt Graham <grahammatt33@gmail.com>
2022-05-09 17:21:13 -0500

    Added win checking on the diagonals

--- ConnectFour/connectfour.cpp
+++ ConnectFour/connectfour.cpp
@@ -186,6 +186,122 @@ public:
 	}
       }
     }
+    // checks if either player wins on a diagonal
+    if (rows[2][5] != ' '
+	&& ((rows[0][1] == rows[1][3] && rows[0][1] == rows[2][5] && rows[0][1] == rows[3][7])
+	|| (rows[1][3] == rows[2][5] && rows[1][3] == rows[3][7] && rows[1][3] == rows[4][9])
+	|| (rows[2][5] == rows[5][7] && rows[2][5] == rows[4][9] && rows[2][5] == rows[5][11]))) {
+      if (rows[2][5] == '0') {
+	color = 0;
+      } else {
+	color = 1;
+      }
+    } else if (rows[2][3] != ' '
+	       && ((rows[1][1] == rows[2][3] && rows[1][1] == rows[3][5]
+		    && rows[1][1] == rows[4][7]) || (rows[2][3] == rows[3][5]
+						     && rows[2][3] == rows[4][7]
+						     && rows[2][3] == rows[5][9]))) {
+      if (rows[2][3] == '0') {
+	color = 0;
+      } else {
+	color = 1;
+      }
+    } else if (rows[2][1] != ' ' && rows[2][1] == rows[3][3] && rows[2][1] == rows[4][5]
+		    && rows[2][1] == rows[5][7]) {
+      if (rows[2][1] == '0') {
+	color = 0;
+      } else {
+	color = 1;
+      }
+    } else if (rows[2][7] != ' '
+	       && ((rows[0][3] == rows[1][5] && rows[0][3] == rows[2][7]
+		    && rows[0][3] == rows[3][9]) || (rows[1][5] == rows[2][7]
+						     && rows[1][5] == rows[3][9]
+						     && rows[1][5] == rows[4][11])
+		   || (rows[2][7] == rows[3][9] && rows[2][7] == rows[4][11]
+		       && rows[2][7] == rows[5][13]))) {
+      if (rows[2][7] == '0') {
+	color = 0;
+      } else {
+	color = 1;
+      }
+    } else if (rows[1][7] != ' '
+	       && ((rows[0][5] == rows[1][7] && rows[0][5] == rows[2][9]
+		    && rows[0][5] == rows[3][11]) || (rows[1][7] == rows[2][9]
+						      && rows[1][7] == rows[3][11]
+						      && rows[2][9] == rows[4][13]))) {
+      if (rows[1][7] == '0') {
+	color = 0;
+      } else {
+	color = 1;
+      }
+    } else if (rows[0][7] != ' ' && rows[0][7] == rows[1][9] && rows[0][7] == rows[2][11]
+		    && rows[0][7] == rows[3][13]) {
+      if (rows[0][7] == '0') {
+	color = 0;
+      } else {
+	color = 1;
+      }
+    } else if (rows[3][1] != ' ' && rows[3][1] == rows[2][3] && rows[3][1] == rows[1][5]
+		    && rows[3][1] == rows[0][7]) {
+      if (rows[3][1] == '0') {
+	color = 0;
+      } else {
+	color = 1;
+      }
+    } else if (rows[3][3] != ' '
+	       && ((rows[4][1] == rows[3][3] && rows[4][1] == rows[2][5]
+		    && rows[4][1] == rows[1][7]) || (rows[3][3] == rows[2][5]
+						     && rows[3][3] == rows[1][7]
+						     && rows[3][3] == rows[0][9]))) {
+      if (rows[3][3] == '0') {
+	color = 0;
+      } else {
+	color = 1;
+      }
+    } else if (rows[3][5] != ' '
+	       && ((rows[5][1] == rows[4][3] && rows[5][1] == rows[3][5]
+		    && rows[5][1] == rows[2][7]) || (rows[4][3] == rows[3][5]
+						     && rows[4][3] == rows[2][7]
+						     && rows[4][3] == rows[1][9])
+		   || (rows[3][5] == rows[2][7] && rows[3][5] == rows[1][9]
+		       && rows[3][5] == rows[0][11]))) {
+      if (rows[3][5] == '0') {
+	color = 0;
+      } else {
+	color = 1;
+      }
+    } else if (rows[3][7] != ' '
+	       && ((rows[5][3] == rows[4][5] && rows[5][3] == rows[3][7]
+		    && rows[5][3] == rows[2][9]) || (rows[4][5] == rows[3][7]
+						     && rows[4][5] == rows[2][9]
+						     && rows[4][5] == rows[1][11])
+		   || (rows[3][7] == rows[2][9] && rows[2][7] == rows[1][11]
+		       && rows[3][7] == rows[0][13]))) {
+      if (rows[3][7] == '0') {
+	color = 0;
+      } else {
+	color = 1;
+      }
+    } else if (rows[4][7] != ' '
+	       && ((rows[5][5] == rows[4][7] && rows[5][5] == rows[3][9]
+		    && rows[5][5] == rows[2][11]) || (rows[4][7] == rows[3][9]
+						      && rows[4][7] == rows[2][11]
+						      && rows[4][7] == rows[1][13]))) {
+      if (rows[4][7] == '0') {
+	color = 0;
+      } else {
+	color = 1;
+      }
+    } else if (rows[5][7] != ' ' && rows[5][7] == rows[4][9] && rows[5][7] == rows[3][11]
+		    && rows[5][7] == rows[2][13]) {
+      if (rows[5][7] == '0') {
+	color = 0;
+      } else {
+	color = 1;
+      }
+    }
+    // checks who won
     if (color == 1) {
       // red wins
       return 1;
