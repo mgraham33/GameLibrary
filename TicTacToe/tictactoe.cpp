0952a76c9a7c3319c6095613fe69791b79690d7b
Matt Graham <grahammatt33@gmail.com>
2022-05-08 20:13:07 -0500

    Removed unneeded color pairs

--- TicTacToe/tictactoe.cpp
+++ TicTacToe/tictactoe.cpp
@@ -41,14 +41,6 @@ void io_init_terminal(void) {
   curs_set(0);
   keypad(stdscr, TRUE);
   start_color();
-  // creates the color pairs
-  init_pair(COLOR_RED, COLOR_RED, COLOR_BLACK);
-  init_pair(COLOR_GREEN, COLOR_GREEN, COLOR_BLACK);
-  init_pair(COLOR_YELLOW, COLOR_YELLOW, COLOR_BLACK);
-  init_pair(COLOR_BLUE, COLOR_BLUE, COLOR_BLACK);
-  init_pair(COLOR_MAGENTA, COLOR_MAGENTA, COLOR_BLACK);
-  init_pair(COLOR_CYAN, COLOR_CYAN, COLOR_BLACK);
-  init_pair(COLOR_WHITE, COLOR_WHITE, COLOR_BLACK);
 }
 
 void printBoard() {
