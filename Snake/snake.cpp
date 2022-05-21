7ca1ec25d804a815014fcc2a9913137fa0ff79c6
Matt Graham <grahammatt33@gmail.com>
2022-05-21 11:38:59 -0500

    Set up files for snake

---
+++ Snake/snake.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <sys/time.h>
+#include <assert.h>
+#include <unistd.h>
+#include <ncurses.h>
+#include <ctype.h>
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+
+void io_init_terminal(void) {
+  initscr();
+  raw();
+  noecho();
+  curs_set(0);
+  keypad(stdscr, TRUE);
+  start_color();
+}
+
+void gameLoop() {
+  
+}
+
+int main(int argc, char *argv[]) {
+  io_init_terminal();
+  // starts the game
+  gameLoop();
+  endwin();
+  return 0;
+}
